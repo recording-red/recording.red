dd7a0a3ffaf6c6be5cfd30f5765fa6cb0c9e1a35
Remi Petitjean <remi.petitjean@gmail.com>
2022-11-20 20:22:19 +0000

    video table

--- src/views/Studio.vue
+++ src/views/Studio.vue
@@ -1,15 +1,18 @@
 <template>
   <Header />
+  <VideoTable />
+
 
 </template>
 
 <script lang="ts">
 import Header from "@/components/studio/Header.vue";
+import VideoTable from "@/components/studio/VideoTable.vue";
 
 export default {
   name: "Studio",
 
-  components: {Header},
+  components: {Header, VideoTable},
 
   props: {},
 };
