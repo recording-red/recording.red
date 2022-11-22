df1901ead0800ba491656b2fd2f2e017bee8be26
Remi Petitjean <remi.petitjean@gmail.com>
2022-11-22 22:18:59 +0000

    cleaning

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,9 @@
 <template>
   <v-app>
-    <v-app>
-      <v-main>
-        <NavBar />
-        <v-container fluid><router-view></router-view></v-container>
-      </v-main>
-    </v-app>
+    <v-main>
+      <NavBar />
+      <v-container fluid><router-view></router-view></v-container>
+    </v-main>
   </v-app>
 </template>
 
